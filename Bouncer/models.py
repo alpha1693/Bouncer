b304c08a209a68a22251c3be1777c8ecdb67db8f
tmwag2811 <tmwag2811@gmail.com>
2017-03-29 19:52:28 -0400

    ParsedLog ready for bulk insert testing

--- Bouncer/models.py
+++ Bouncer/models.py
@@ -1,7 +1,6 @@
 from django.db import models, transaction
 import re
 
-
 # class to store Apache log files using Common Log Format
 class ParsedLog(models.Model):
 	ip_address = models.CharField(max_length=39)
@@ -31,8 +30,3 @@ class ParsedLog(models.Model):
 		self.request_line = tokens[4]
 		self.http_status = tokens[5]
 		self.num_bytes = tokens[6]
-
-
-test_log = '64.242.88.10 - - [07/Mar/2004:16:05:49 -0800] "GET /twiki/bin/edit/Main/Double_bounce_sender?topicparent=Main.ConfigurationVariables HTTP/1.1" 401 12846'
-parsed = ParsedLog(test_log)
-print parsed
