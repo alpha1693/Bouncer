87ecfea3c6d18afad9688dfc488ac40561992849
Solomon <Moris@rpi.edu>
2017-03-28 21:07:53 -0400

    views.py Checks for uploaded file
    Checks for a file and gives an error if it doesnt exist. Gives the name
    of the file if succesful.

--- Bouncer/views.py
+++ Bouncer/views.py
@@ -4,16 +4,28 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
 def simple_upload(request):
-    if request.method == 'POST' and request.FILES['myfile']:
+    error = ""
+    uploaded_file_url = ""
+    if len(request.FILES) == 0:
+        error = "No file uploaded\n"
+
+
+    elif request.method == 'POST' and request.FILES['myfile']:
+        #needs error checks
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
-        return render(request, 'templates/simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
+        
+
+    return render(request, 'templates/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url,
+            'error' : error
+    })
     return render(request, 'templates/simple_upload.html')
 
+def home(request):
+    return render(request, 'templates/base.html')
 
 def hello(request):
     return HttpResponse("Hello world")
