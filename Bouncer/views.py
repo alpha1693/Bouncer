389f9952aaa79d853cfa30b001a3488fd0c2560c
tmwag2811 <tmwag2811@gmail.com>
2017-03-29 22:13:07 -0400

    Parse and bulk insert from log upload

--- Bouncer/views.py
+++ Bouncer/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
+from .models import *
+
 def simple_upload(request):
 
     if request.method == 'POST' and len(request.FILES) == 0:
@@ -20,6 +22,16 @@ def simple_upload(request):
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
         
+        log_file = myfile.chunks()
+        parsed_logs = []
+        for line in log_file:
+            tokens = ParsedLog.parse_line(line)
+            if tokens == None:
+                continue
+            else:
+                parsed_logs.append(ParsedLog(tokens))
+
+        ParsedLog.objects.bulk_create(parsed_logs)
 
         return render(request, 'templates/simple_upload.html', {
             'uploaded_file_url': uploaded_file_url,
