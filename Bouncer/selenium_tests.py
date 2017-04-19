e2d2e58dbf3f591c12f96cf8b83217ad1fffda8a
tmwag2811 <tmwag2811@gmail.com>
2017-04-19 16:21:53 -0400

    improve test for log upload

--- Bouncer/selenium_tests.py
+++ Bouncer/selenium_tests.py
@@ -1,5 +1,5 @@
-import unittest
-from django.test import Client
+import unittest, sys, re, time
+from settings import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -8,7 +8,7 @@ import sys
 desired_drivers = []
 if sys.platform == 'linux2':
 	desired_drivers.append("webdriver.Chrome('/usr/bin/chromedriver')")
-	# desired_drivers.append("webdriver.Firefox('/usr/bin/geckodriver')")
+	# desired_drivers.append("webdriver.Firefox(firefox_binary=binary)")
 
 elif sys.platform == 'win32':
 	desired_drivers.append("webdriver.Chrome('../selenium_browser_drivers/chromedriver.exe')")
@@ -23,25 +23,21 @@ class current_test_case(unittest.TestCase):
 
 			self.driver = eval(driver_instance)
 
-
-
 	def tearDown(self):
 		self.driver.quit()
 		print("quit " + str(self.driver) )
 
-
-	def testExample(self):
-		self.driver.get("http://www.google.com/")
-		assert(self.driver.title == "Google")
-
 	def testLogUpload(self):
 		self.driver.get('http://127.0.0.1:8000/simple')
-		file_path = '../testlog.txt'
+		file_path = BASE_DIR + '/testlog.txt'
+
 		element = self.driver.find_element_by_id('myfile')
 		element.send_keys(file_path)
-
-		# validate...
-
+		element.submit()
+		time.sleep(1)
+		element = self.driver.find_element_by_id('post_msg')
+		regex = 'File \w+\.txt uploaded'
+		self.assertTrue(re.match(regex, element.text))
 
 if __name__ == '__main__':
     unittest.main()
