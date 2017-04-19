4507fd160eb735fbfddcc2d7a7adcf362a0deba7
tmwag2811 <tmwag2811@gmail.com>
2017-04-19 17:17:30 -0400

    move parsedlog to account models

--- Bouncer/models.py
+++ Bouncer/models.py
@@ -1,20 +1,3 @@
 from django.db import models, transaction
 from django.contrib.auth.models import User
 
-import re
-
-
-class User(models.Model):
-	email = models.EmailField()
-	password = models.CharField(max_length = 40)
-
-
-# class to store Apache log files using Common Log Format
-class ParsedLog(models.Model):
-	ip_address = models.CharField(max_length=39)
-	rfc_id = models.CharField(max_length=39)
-	user_id = models.CharField(max_length=39)
-	date_time = models.CharField(max_length=29)
-	request_line = models.TextField()
-	http_status = models.CharField(max_length=3)
-	num_bytes = models.IntegerField(blank=True)
--- Bouncer/views.py
+++ Bouncer/views.py
@@ -48,7 +48,7 @@ def simple_upload(request):
             tokens = parse_line(line)
             # parse_line returns None if regex fails to match
             if tokens != None:
-                parsed_log = ParsedLog(ip_address = tokens[0],rfc_id = tokens[1],user_id = tokens[2],date_time = tokens[3],request_line = tokens[4],http_status = tokens[5],num_bytes = tokens[6])
+                parsed_log = ParsedLog(owner = request.user, ip_address = tokens[0],rfc_id = tokens[1],user_id = tokens[2],date_time = tokens[3],request_line = tokens[4],http_status = tokens[5],num_bytes = tokens[6])
                 parsed_logs.append(parsed_log)
         # Bulk insert into database
         ParsedLog.objects.bulk_create(parsed_logs)
